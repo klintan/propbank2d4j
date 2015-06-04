ffa11de15ac42f2ebe8f9e77d0d0776cb33aa7ca
Andreas Klintberg <andreas.klintberg@meltwater.com>
2015-06-04 19:43:30 +0200

    mode 0 and mode 2 now works

--- propbank2d4j.py
+++ propbank2d4j.py
@@ -31,6 +31,7 @@ class probank2d4j:
             raw_sentence[predicate_index] = ["<"+instance.roleset+">"+raw_sentence[predicate_index][0]+"</"+instance.roleset+">" ]
 
     def addArgumentLabel(self, raw_sentence, instance):
+        raw_sentence_preflat = deepcopy(self.raw_sentence)
         for (argloc, argid) in instance.arguments:
             arguments_list =[]
             if (argloc.select(instance.tree).label() != "-NONE-"):
@@ -41,11 +42,14 @@ class probank2d4j:
                         arguments_list.append(arg)
 
             new_sentence = [item for sublist in raw_sentence_preflat for item in sublist]
-            seguence_index = self.getIndexOfSequence(arguments_list,new_sentence)
+            sequence_index = self.getIndexOfSequence(arguments_list,new_sentence)
 
-            if len(seguence_index)>0:
-                raw_sentence[seguence_index[0][0]].insert(0,"<"+argid+">")
-                raw_sentence[seguence_index[0][1]-1].append("</"+argid+">")
+            print sequence_index
+            print len(self.raw_sentence)
+
+            if len(sequence_index)>0:
+                self.raw_sentence[sequence_index[0][0]].insert(0,"<"+argid+">")
+                self.raw_sentence[sequence_index[0][1]-1].append("</"+argid+">")
 
     def getIndexOfSequence(self, sequence, activelist):
             return [(i, i+len(sequence)) for i in range(len(activelist)) if activelist[i:i+len(sequence)] == sequence]
@@ -65,39 +69,63 @@ class probank2d4j:
         return wordlist
 
     def run(self):
+        print "mode"
+        print self.mode
+
         for i in range(0, 9351):
             #print the instance number
-            #print i
+            print i
             #next instance
             next_instance = self.instances[i+1]
             #current instance
             instance = self.instances[i]
             #Mode 0: This mode is default, it will be one predicate and argument structure per sentence.
             if(self.mode==0):
-                print "mode 0"
-                self.raw_sentence = wordlistFromTree(instance)
-                raw_sentence_preflat = deepcopy(self.raw_sentence)
+                if(self.x == 0):
+                    self.raw_sentence = self.wordlistFromTree(instance)
+
+                    if(self.arguments):
+                        #add labels for argument spans
+                        self.addArgumentLabel(self.raw_sentence,instance)
+
+                    if(self.predicate):
+                        #add the predicate/roleset label
+                        self.addPredicateLabel(self.raw_sentence, instance)
+
+                    self.saveAnnotatedSentence(self.raw_sentence)
+                    self.x = 1
+
+                print instance.sentnum
+                print next_instance.sentnum
+
+                if(instance.sentnum == next_instance.sentnum ):
+                    self.x = 1
+                else:
+                    self.x = 0
 
             #Mode 1: Several predicate and argument structures in one sentence which migth overlap
-            elif(self.mode == 1):
-                print "mode 1"
+            elif(self.mode==1):
                 if self.x == 0:
-                    self.raw_sentence = wordlistFromTree(instance)
-                    raw_sentence_preflat = deepcopy(self.raw_sentence)
+                    self.raw_sentence = self.wordlistFromTree(instance)
                     self.x=1
 
                 #save all predicates for one file and their position
                 if(instance.sentnum==next_instance.sentnum):
-                    #add labels for argument spans
-                    self.addArgumentLabel(self.raw_sentence,instance)
-                    #add the predicate/roleset label
-                    self.addPredicateLabel(self.raw_sentence, instance)
 
+                    if(self.arguments):
+                        #add labels for argument spans
+                        self.addArgumentLabel(self.raw_sentence,instance)
+
+                    if(self.predicate):
+                        #add the predicate/roleset label
+                        self.addPredicateLabel(self.raw_sentence, instance)
                 else:
-                    #add labels for argument spans
-                    self.addArgumentLabel(self.raw_sentence,instance)
-                    #add the predicate/roleset label
-                    self.addPredicateLabel(self.raw_sentence, instance)
+                    if(self.arguments):
+                        #add labels for argument spans
+                        self.addArgumentLabel(self.raw_sentence,instance)
+                    if(self.predicate):
+                        #add the predicate/roleset label
+                        self.addPredicateLabel(self.raw_sentence, instance)
 
                     self.saveAnnotatedSentence(self.raw_sentence)
                     #clear all variables
@@ -106,55 +134,30 @@ class probank2d4j:
 
                     #set x to 0 if we have a new file
                     if(next_instance.fileid != instance.fileid):
-                        x = 0
-                        raw_sentence = []
+                        self.x = 0
+                        self.raw_sentence = []
 
             #All predicates arguments in a sentence, multiple identical sentences different annotations
             elif(self.mode == 2):
-                print "mode 2"
+                self.raw_sentence = self.wordlistFromTree(instance)
+
+                if(self.arguments):
+                    #add labels for argument spans
+                    self.addArgumentLabel(self.raw_sentence,instance)
+
+                if(self.predicate):
+                    #add the predicate/roleset label
+                    self.addPredicateLabel(self.raw_sentence, instance)
+
+                self.saveAnnotatedSentence(self.raw_sentence)
+
             else:
                 print "mode does not exist"
 
 
 
 if __name__ == "__main__":
-    pb2d4j = probank2d4j()
-    #iterate through all the instances in the dataset
-    #9351 is the max
-    for i in range(0, 9351):
-        print i
-        next_instance = pb2d4j.instances[i+1]
-        #active instance
-        instance = pb2d4j.instances[i]
-        if pb2d4j.x == 0:
-            for s in instance.tree.subtrees(lambda t: t.height() == 2):
-                if (s.label() != "-NONE-"):
-                    pb2d4j.raw_sentence.append(s.leaves())
-            raw_sentence_preflat = deepcopy(pb2d4j.raw_sentence)
-        pb2d4j.x=1
+    pb2d4j = probank2d4j(mode=0)
 
-        #save all predicates for one file and their position
-        if(instance.sentnum==next_instance.sentnum):
-            #add labels for argument spans
-            pb2d4j.addArgumentLabel(pb2d4j.raw_sentence,instance)
-            #add the predicate/roleset label
-            pb2d4j.addPredicateLabel(pb2d4j.raw_sentence, instance)
-
-        else:
-            #add labels for argument spans
-            pb2d4j.addArgumentLabel(pb2d4j.raw_sentence,instance)
-            #add the predicate/roleset label
-            pb2d4j.addPredicateLabel(pb2d4j.raw_sentence, instance)
-
-            pb2d4j.saveAnnotatedSentence(pb2d4j.raw_sentence)
-            #clear all variables
-            pb2d4j.x = 0
-            pb2d4j.raw_sentence = []
-
-        #set x to 0 if we have a new file
-        if(next_instance.fileid != instance.fileid):
-            x = 0
-            raw_sentence = []
-
-
+    pb2d4j.run()
 
